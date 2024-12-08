2b887abbc6b495a753d662d65d05fd6d61fc4a2c
papercoding22 <john.ly997@gmail.com>
2024-12-08 17:34:47 +0700

    Add maxArea function to solve the Container with Most Water problem and corresponding tests

---
+++ src/arrays/container-with-most-water.ts
@@ -0,0 +1,32 @@
+/**
+ * @author: Paper Coding
+ * @link: https://leetcode.com/problems/container-with-most-water/description/
+ * Pattern: Arrays - Two Pointers
+ * Description: Container with Most Water
+ * - Given an integer array height, representing the height of the bars.
+ * - Find the two bars that form a container that holds the most water.
+ * - Return the maximum area of the container.
+ * For Example:
+ * Input: [1,8,6,2,5,4,8,3,7]
+ * Output: 49, Explanation: The maximum area is formed by bars 8 and 7.
+ */
+function maxArea(height: number[]): number {
+  let pointerLeft = 0;
+  let pointerRight = height.length - 1;
+  let maxArea = 0;
+  while (pointerLeft < pointerRight) {
+    const area =
+      Math.min(height[pointerLeft], height[pointerRight]) *
+      (pointerRight - pointerLeft);
+    maxArea = Math.max(area, maxArea);
+    if (height[pointerLeft] < height[pointerRight]) {
+      pointerLeft++;
+    } else {
+      pointerRight--;
+    }
+  }
+
+  return maxArea;
+}
+
+export { maxArea };
---
+++ src/arrays/tests/container-with-most-water.test.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest';
+import { maxArea } from '../container-with-most-water';
+
+describe('Container with Most Water', () => {
+  it('should return the maximum area of the container', () => {
+    const height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+    expect(maxArea(height)).toEqual(49);
+  });
+});
