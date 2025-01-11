bed5c70c023c34e788f269ebdfe60c2a264f8dff
papercoding22 <john.ly997@gmail.com>
2025-01-11 16:31:28 +0700

    VN25-31: Update sortColors function documentation with problem details and algorithm explanation

--- src/arrays/sort-colors.ts
+++ src/arrays/sort-colors.ts
@@ -1,5 +1,17 @@
 /**
- Do not return anything, modify nums in-place instead.
+ * Link: https://leetcode.com/problems/sort-colors/
+ * Level: Easy
+ * Pattern: Sorting
+ * Description: Sort Colors
+ * - Sort an array of 0's, 1's and 2's in linear time complexity.
+ * - You are not allowed to use the inbuilt sort function.
+ * - The algorithm should run in O(n) time complexity.
+ * Intuition:
+ * - Create a bucket array to store the count of each element
+ * - Loop through the array and increment the count of each element
+ * - Loop through the bucket array and update the original array
+ * Time complexity: O(n)
+ * Space complexity: O(1)
  */
 function sortColors(nums: number[]): void {
   const buckets = [0, 0, 0];
