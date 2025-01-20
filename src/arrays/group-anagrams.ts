26a5caa64ea8ca587d3a7ab7d7e319eca3219b4f
papercoding22 <john.ly997@gmail.com>
2025-01-20 23:36:20 +0700

    Enhance documentation for groupAnagrams function with intuition, time complexity, and space complexity details

--- src/arrays/group-anagrams.ts
+++ src/arrays/group-anagrams.ts
@@ -7,6 +7,12 @@
  * For Example:
  * Input: ["eat","tea","tan","ate","nat","bat"]
  * Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ * Intuition:
+ * - We can group anagrams by summing the ASCII values of the characters in the word.
+ * - We can use a map to store the sum of the ASCII values as the key and the words as the value.
+ * - We can then return the values of the map.
+ * Time Complexity: O(n * m) where n is the number of words and m is the length of the longest word.
+ * Space Complexity: O(n) where n is the number of words.
  */
 
 function groupAnagrams(strs: string[]): string[][] {
