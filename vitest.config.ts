156dd5e5a962f31d6b961cb9753fd512a1a2dbfe
papercoding22 <john.ly997@gmail.com>
2025-01-22 11:28:13 +0700

    Enhance TypeScript configuration for Vitest by adding global types and test file inclusion

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    include: ['**.test.ts'],
     setupFiles: ['setup.vitest.ts'],
+    globals: true,
   },
 });
