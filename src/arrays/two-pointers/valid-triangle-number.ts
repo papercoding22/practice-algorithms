7a5ea24afda38ecb67ec2713bdeece52b83d22f2
papercoding22 <john.ly997@gmail.com>
2025-02-07 11:29:04 +0700

    VN25-42: Optimize triangle number calculation by replacing nested loops with a two-pointer approach

--- src/arrays/two-pointers/valid-triangle-number.ts
+++ src/arrays/two-pointers/valid-triangle-number.ts
@@ -1,34 +1,54 @@
-function countValidTriangles(nums: number[], currentIndex: number) {
-  let slowPointer = currentIndex + 1;
+// function countValidTriangles(nums: number[], currentIndex: number) {
+//   let slowPointer = currentIndex + 1;
 
-  // increase count by 1 when 3 sides are valid
-  let count = 0;
+//   // increase count by 1 when 3 sides are valid
+//   let count = 0;
 
-  while (slowPointer < nums.length - 1) {
-    let fastPointer = slowPointer + 1;
+//   while (slowPointer < nums.length - 1) {
+//     let fastPointer = slowPointer + 1;
 
-    while (fastPointer < nums.length) {
-      if (nums[currentIndex] + nums[slowPointer] > nums[fastPointer]) {
-        count += 1;
-      } else {
-        break;
-      }
+//     while (fastPointer < nums.length) {
+//       if (nums[currentIndex] + nums[slowPointer] > nums[fastPointer]) {
+//         count += 1;
+//       } else {
+//         break;
+//       }
 
-      fastPointer += 1;
-    }
+//       fastPointer += 1;
+//     }
 
-    slowPointer += 1;
-  }
+//     slowPointer += 1;
+//   }
 
-  return count;
-}
+//   return count;
+// }
+
+// function triangleNumber(nums: number[]): number {
+//   const sortedNums = nums.sort((a, b) => a - b);
+
+//   let count = 0;
+//   for (let i = 0; i < sortedNums.length - 2; i++) {
+//     count += countValidTriangles(sortedNums, i);
+//   }
+
+//   return count;
+// }
 
 function triangleNumber(nums: number[]): number {
-  const sortedNums = nums.sort((a, b) => a - b);
+  const sortedNums = nums.sort((a, b) => b - a);
 
   let count = 0;
   for (let i = 0; i < sortedNums.length - 2; i++) {
-    count += countValidTriangles(sortedNums, i);
+    let left = i + 1;
+    let right = sortedNums.length - 1;
+    while (left < right) {
+      if (sortedNums[left] + sortedNums[right] > sortedNums[i]) {
+        count += right - left;
+        left++;
+      } else {
+        right--;
+      }
+    }
   }
 
   return count;
